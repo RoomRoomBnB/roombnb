fe3d7215690791a51e76be4071408d844b64cc07
noeyodeel <sko8044@gmail.com>
2024-02-07 15:38:38 +0900

    :tada:

---
+++ src/main/java/com/sparta/roombnb/RoombnbApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.roombnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoombnbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoombnbApplication.class, args);
+    }
+
+}
